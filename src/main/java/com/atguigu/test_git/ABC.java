40e428e6defcd18f07636383032d0b3addd6b075
“李志强” <1020107680@qq.com>
2021-02-02 20:15:35 +0800

    ABC类中输出增加了master

--- src/main/java/com/atguigu/test_git/ABC.java
+++ src/main/java/com/atguigu/test_git/ABC.java
@@ -2,6 +2,6 @@ package com.atguigu.test_git;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("222222222");
+        System.out.println("222222222master");
     }
 }
